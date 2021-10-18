cec0eb4db8d899b31d6903ac02dfd5414a711c38
Onuryontar <onuryontar@live.com>
2021-10-19 00:15:00 +0300

    18.10.2021 Ders Sonu 2

---
+++ src/app.js
@@ -0,0 +1,11 @@
+console.log("Merhaba Kodlama.io")
+
+let konutKredileri = ["Evlak Kredisi","Tüketici Kredisi" , "Araba Kredisi","İhtiyaç Kredisi"]
+
+console.log("<ul>")
+for(let i =0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
